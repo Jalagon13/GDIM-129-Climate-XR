95aa48c116f53e6dd4d46959018d99681e30d7a5
AlainaAK <alaina.klaes@gmail.com>
2023-11-12 22:36:03 -0800

    IMPLEMENTED REELING!!! Also, moved bouncing behavior out of WaterCollisionBehavior and into Hook.
    Reeling mechanic based on whether controller is moving enough between FixedUpdates! I may also later have it check if controllers are in a bounding box of the reel or something, to make sure movement is within the space where they would be reeling.

--- Assets/_Project/_Scripts/Physical/Water/WaterCollisionBehavior.cs
+++ Assets/_Project/_Scripts/Physical/Water/WaterCollisionBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,58 +7,18 @@ namespace MagnetFishing
 {
     public class WaterCollisionBehavior : MonoBehaviour
     {
-        public static bool alreadyAnimating = false;
+        private static GameObject currentBobber;
         private void OnCollisionEnter(Collision collision)
         {
-            if (collision.gameObject.CompareTag("Bobber"))
+            if (collision.gameObject.CompareTag("Bobber") && (currentBobber == null || currentBobber != collision.gameObject))
             {
+                currentBobber = collision.gameObject;
                 Rigidbody thingRigidbody = collision.gameObject.transform.GetComponent<Rigidbody>();
                 thingRigidbody.velocity = new Vector3(0, 0, 0);
                 Debug.Log("Bobber stopped in its tracks! Bouncing now:");
-                if (alreadyAnimating == false)
-                {
-                    alreadyAnimating = true;
-                    StartCoroutine(BobberBounceCoroutine(collision.gameObject));
-                    alreadyAnimating = false;
-                    Debug.Log("Bobber done bouncing!");
-                }
-                else
-                {
-                    Debug.Log("Bobber already bouncing!");
-                }
+                Hook currentHook = currentBobber.GetComponent<Hook>();
+                currentHook.EnterWaterBehavior();
             }
-        }
-
-        private void OnTriggerEnter(Collider thingEntering)
-        {
-
-        }
-
-        private IEnumerator BobberBounceCoroutine(GameObject bobber)
-        {
-            Vector3 originalPosition = bobber.transform.position;
-
-            while (bobber.transform.position.y < originalPosition.y + 0.5f && bobber != null)
-            {
-                bobber.transform.position = bobber.transform.position + new Vector3(0, 0.05f, 0);
-                yield return null;
-            }
-            while (bobber.transform.position.y > originalPosition.y - 0.3f)
-            {
-                bobber.transform.position = bobber.transform.position - new Vector3(0, 0.05f, 0);
-                yield return null;
-            }
-            while(bobber.transform.position.y < originalPosition.y + 0.1f)
-            {
-                bobber.transform.position = bobber.transform.position + new Vector3(0, 0.05f, 0);
-                yield return null;
-            }
-            while (bobber.transform.position.y > originalPosition.y-0.005f)
-            {
-                bobber.transform.position = bobber.transform.position - new Vector3(0, 0.05f, 0);
-                yield return null;
-            }
-
         }
     }
 }
